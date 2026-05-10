7068d70f33cbda1c42b2969aeb9e5f3552eaf07c
vanvani41 <iberezuk523@gmail.com>
2026-05-10 18:46:23 +0300

    Initial not commit

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 http = require('http')
+fs = require('fs')
+path = require('path')
+url = require('url')
+const dataPath = path.join(__dirname, "data")
+
+function getAllJokes(req, res){
+    /*datadir = path.join(__dirname, "data")
+    fsdirreaded = fs.readdirSync(datadir)
+    data = {}
+    for (i = 0; i < fsdirreaded; i++)
+    {
+        fs.readFileSync(i)
+        data.push(i)
+    }
+    res.end(data)*/
+    let dir = fs.readdirSync(dataPath)
+    let allJokes = []
+
+    for(let i = 0; i < dir.length; i++)
+    {
+        let file = fs.readFileSync(path.join(dataPath, i+'.json'))
+        let jokeJSON = Buffer.from(file).toString()
+        let joke = JSON.parse(jokeJSON)
+        joke.id = i;
+
+        allJokes.push(joke)
+    }
+    res.end(JSON.stringify(allJokes))
+}
 
 const server = http.createServer((req, res) => {
     if(req.url == '/jokes' && req.method == "GET")
