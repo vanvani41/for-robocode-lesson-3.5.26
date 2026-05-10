78632a50663aaae53f74f183d42f97a0071da90a
vanvani41 <iberezuk523@gmail.com>
2026-05-10 19:16:53 +0300

    asd

--- index.js
+++ index.js
@@ -5,15 +5,6 @@ url = require('url')
 const dataPath = path.join(__dirname, "data")
 
 function getAllJokes(req, res){
-    /*datadir = path.join(__dirname, "data")
-    fsdirreaded = fs.readdirSync(datadir)
-    data = {}
-    for (i = 0; i < fsdirreaded; i++)
-    {
-        fs.readFileSync(i)
-        data.push(i)
-    }
-    res.end(data)*/
     let dir = fs.readdirSync(dataPath)
     let allJokes = []
 
@@ -29,10 +20,32 @@ function getAllJokes(req, res){
     res.end(JSON.stringify(allJokes))
 }
 
+function addJoke(req, res){
+    let data = ''
+    req.on('data', function(chunk){
+        data += chunk
+    })
+    req.on('end', function(){
+        let joke = JSON.parse(data)
+        joke.likes = 0
+        joke.dislikes = 0
+
+        let dir = fs.readdirSync(dataPath)
+        let fileName = dir.length+'.json'
+        let filePath = path.join(dataPath, fileName)
+        fs.writeFileSync(filePath, JSON.stringify(joke))
+        res.end()
+    })
+}
+
 const server = http.createServer((req, res) => {
     if(req.url == '/jokes' && req.method == "GET")
     {
         getAllJokes(req, res)
+    }
+    if(req.url == '/jokes' && req.method == "POST")
+    {
+        addJoke(req, res)
     }
 })
 server.listen(2637)
